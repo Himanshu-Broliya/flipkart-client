17bd5a45d2479f5c1bf4b761975d3ecbd970ee23
Himanshu <himanshubroliya2002@gmail.com>
2024-04-17 13:40:05 +0530

    first commit

--- src/assets/index.js
+++
@@ -1,8 +0,0 @@
-import logo from './logo.png'
-import flipkart from './flipkart.png'
-import roundIcon from './roundIcon.png'
-
-
-export {
-    logo,flipkart,roundIcon
-}
--- src/redux/constant.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TO_CART = "addToCart";
-export const ADD_TO_CART_ERROR = "addToCartERROR";
-export const REMOVE_FROM_CART = "removeFromCart";
-
